f4aa3086be51a9e984f14c32bcf313f0e06ce67e
xueluwei <xueluwei@deojian.dev>
2020-09-15 10:54:21 +0800

    1.remove useless things

--- app/src/main/java/com/example/xlwapp/fragment/examplefragment/GameWonFragment.kt
+++ app/src/main/java/com/example/xlwapp/fragment/examplefragment/GameWonFragment.kt
@@ -9,28 +9,7 @@ import androidx.navigation.findNavController
 import com.example.xlwapp.R
 import com.example.xlwapp.databinding.FragmentGameWonBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GameWonFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GameWonFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -73,28 +52,4 @@ class GameWonFragment : Fragment() {
         return intent
     }
 
-
-
-
-
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameWonFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameWonFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
